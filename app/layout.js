a1fe01e6f4de8f2f70ad3895c5d53144bb8e4e6c
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-09-14 20:26:15 +0530

    navbar and footer

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,10 +22,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
+        <div className="container mx-auto min-h-[87.8vh]">
+          {children}
+        </div>
+        <Footer />
       </body>
     </html>
   );
